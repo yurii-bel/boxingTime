c0d76162906d534b68d653559645ce7cb3662ab1
Yurii <yuriibel888@gmail.com>
2023-07-18 20:46:00 +0300

    feat: change

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ root.render(
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={redirectUri}
+      redirectUri={window.location.origin}
     >
       <ThemeProvider>
         <App />
