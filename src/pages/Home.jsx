bb2e581ab0e57c11390b4d25aae3233e49ad49de
Yurii <yuriibel888@gmail.com>
2023-07-18 20:21:54 +0300

    style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div className="absolute top-0 left-0 w-full h-full flex gap-4 flex-col items-center justify-center">
         <h1 className="text-red-900 bg-gray-900/50 text-xl font-bold p-2 rounded-sm md:text-4xl">
           <span className="bg-white px-1 rounded-sm">Boxing</span>
-          <span className="text-blue-500">Time</span>
+          <span className="ml-1 text-blue-500">Time</span>
         </h1>
         <p className="text-white text-md bg-blue-900/50 px-1 rounded-sm md:text-xl md:px-2">
           Your Ultimate Source for Boxing Schedules and More!
